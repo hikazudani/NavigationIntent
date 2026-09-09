cb396fc3bc725ffa5c659558270514c958f5965b
hikazudani <lhjundi@proton.me>
2026-09-09 19:42:02 -0300

    feat: add IntentScreen e Parameter Screen

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/navigationintent/ui/composable/screen/IntentScreen.kt
@@ -0,0 +1,44 @@
+package br.edu.ifsp.scl.sc3038432.navigationintent.ui.composable.screen
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.ifsp.scl.sc3038432.navigationintent.ui.theme.NavigationIntentTheme
+
+@Composable
+fun IntentScreen(modifier: Modifier) {
+    val parameter by remember { mutableStateOf("text") }
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(text = parameter)
+    }
+}
+
+@Preview(
+    name = "Light Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "Dark Mode",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun IntentScreenPreview() {
+    NavigationIntentTheme {
+        // Usando o Surface somente para dar cor de fundo no preview. Sem ele, o fundo fica
+        // transparente e não dá para ver o preview em tema escuro.
+        Surface {
+            IntentScreen(Modifier)
+        }
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/navigationintent/ui/composable/screen/ParameterScreen.kt
@@ -0,0 +1,46 @@
+package br.edu.ifsp.scl.sc3038432.navigationintent.ui.composable.screen
+
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.ifsp.scl.sc3038432.navigationintent.ui.theme.NavigationIntentTheme
+
+@Composable
+fun ParameterScreen(modifier: Modifier) {
+    var parameter by remember { mutableStateOf("") }
+    Column(modifier = modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = parameter,
+            label = { Text("Parameter") },
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { parameter = it })
+    }
+}
+
+@Preview(
+    name = "Light Mode", showBackground = true, uiMode = UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "Dark Mode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun ParameterScreenPreview() {
+    NavigationIntentTheme {
+        // Usando o Surface somente para dar cor de fundo no preview. Sem ele, o fundo fica
+        // transparente e não dá para ver o preview em tema escuro.
+        Surface {
+            ParameterScreen(Modifier)
+        }
+    }
+}
