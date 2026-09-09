003c39d85e1eaac7d4ecb6022fd6bdba4120ab63
hikazudani <lhjundi@proton.me>
2026-09-09 20:07:39 -0300

    feat: add MainTopAppBar

--- app/src/main/java/br/edu/ifsp/scl/sc3038432/navigationintent/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/navigationintent/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.edu.ifsp.scl.sc3038432.navigationintent.ui.composable.component.MainTopAppBar
 import br.edu.ifsp.scl.sc3038432.navigationintent.ui.composable.screen.IntentScreen
 import br.edu.ifsp.scl.sc3038432.navigationintent.ui.theme.NavigationIntentTheme
 
@@ -20,7 +22,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationIntentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = ::MainTopAppBar
+                    ) { innerPadding ->
                     IntentScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
