65cc39ce8ec3b79b8bf534ff727455ac9f9ad396
hikazudani <lhjundi@proton.me>
2026-09-09 20:26:34 -0300

    feat: add MainNavHost

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/navigationintent/navigation/MainNavHost.kt
@@ -0,0 +1,26 @@
+package br.edu.ifsp.scl.sc3038432.navigationintent.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import br.edu.ifsp.scl.sc3038432.navigationintent.ui.composable.screen.IntentScreen
+import br.edu.ifsp.scl.sc3038432.navigationintent.ui.composable.screen.ParameterScreen
+
+@Composable
+fun MainNavHost(navHostController: NavHostController, modifier: Modifier) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.IntentScreen.route
+    ) {
+        // Nós que fazem parte do grafo
+        composable(route = Screen.IntentScreen.route) {
+            IntentScreen(modifier)
+        }
+
+        composable(route = Screen.ParameterScreen.route) {
+            ParameterScreen(modifier)
+        }
+    }
+}
